e7185145067e3a6df5eb567f440334fc30a60c8b
jacobromero <jacob22romero@gmail.com>
2017-01-08 17:49:14 -0800

    Initial Commit.

---
+++ HelloWorldJava/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jacob Romero on 1/6/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HelloWorldNode/HelloWorld.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Jacob Romero on 1/8/2017.
+ */
+console.log("Hello World!");
